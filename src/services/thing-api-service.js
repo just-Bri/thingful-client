5e77e92c047cfe00ee3f5baa5e0e8f14e72d83b4
reifnotreef <bridgerrhammond@gmail.com>
2019-12-07 14:40:10 -0600

    syncing chekpoint4

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,10 +1,12 @@
-import TokenService from '../services/token-service';
-import config from '../config';
+import config from "../config";
+import TokenService from "./token-service";
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
-      headers: {},
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
@@ -12,35 +14,37 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
-        authorization: `basic ${TokenService.getAuthToken()}`,
-      },
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
   },
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
-      headers: { authorization: `basic ${TokenService.getAuthToken()}` },
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
   },
   postReview(thingId, text, rating) {
     return fetch(`${config.API_ENDPOINT}/reviews`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
-        authorization: `basic ${TokenService.getAuthToken()}`,
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`
       },
       body: JSON.stringify({
         thing_id: thingId,
         rating,
-        text,
-      }),
+        text
+      })
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
-  },
+  }
 };
 
 export default ThingApiService;
--- src/services/token-service.js
+++ src/services/token-service.js
@@ -1,4 +1,4 @@
-import config from '../config';
+import config from "../config";
 
 const TokenService = {
   saveAuthToken(token) {
@@ -15,7 +15,7 @@ const TokenService = {
   },
   makeBasicAuthToken(userName, password) {
     return window.btoa(`${userName}:${password}`);
-  },
+  }
 };
 
 export default TokenService;
